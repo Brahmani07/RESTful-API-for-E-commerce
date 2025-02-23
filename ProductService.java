baa332ca3a0d1e1de0aa7004352e3dac738ae48d
Brahmani07 <brahmani0107@gmail.com>
2025-02-23 15:01:18 -0600

    Create ProductService.java

---
+++ ProductService.java
@@ -0,0 +1,21 @@
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Product saveProduct(Product product) {
+        return repository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        repository.deleteById(id);
+    }
+}
